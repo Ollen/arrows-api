d822e76ed0f3abfff1405f3b5eb2449f4151ad58
Allendale Nato <natoallendale@gmail.com>
2017-05-13 20:43:22 +0800

    Move models in db/ dir

---
+++ server/db/config/env.js
@@ -0,0 +1,13 @@
+/**
+ * DATABASE_NAME
+ * DATABASE_USERNAME
+ * DATABASE_PASSWORD
+ * 
+ * host: DATBASE_HOST
+ * port: DATABASE_PORT
+ * dialect: DATABASE_DIALECT
+ * 
+ * define: {
+ *    timestamps: false
+ * }
+ */
